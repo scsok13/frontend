b1ea3220fe3ff92637e80e27cf0daae4faf1f22f
hyeonchaeshin <scsoks@gmail.com>
2022-07-15 11:23:52 +0900

    test

--- my-depl/src/setupProxy.js
+++ my-depl/src/setupProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-module.exports = (app) => {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://35.88.148.173/',
-      chageOrigin: true,
-    })
-  );
-};
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+// module.exports = (app) => {
+//   app.use(
+//     '/api',
+//     createProxyMiddleware({
+//       target: 'http://35.88.148.173/',
+//       chageOrigin: true,
+//     })
+//   );
+// };
